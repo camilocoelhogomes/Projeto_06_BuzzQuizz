701da120d96f0f121b222d88f968c1847fa2c5e9
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-11 18:17:03 -0300

    Mobile Layou of first page quizzCreation Implemented

--- scripts/script.js
+++ scripts/script.js
@@ -64,4 +64,47 @@ function renderQuizzListPage() {
     quizzListGetAllQuizzes();
 }
 
-renderQuizzListPage()
+function renderQuizzCreationPage() {
+
+    document.querySelector('.style').href = './styles/quizzCreation.css'
+
+    document.querySelector('main').innerHTML = `
+        <header class="main-header">
+            <h1 class="main-title">BuzzQuizz</h1>
+        </header>
+
+        <article class='new-quizz-beggin'>
+
+            <header class="new-quizz-header">
+                <h2 class="new-quizz-title">Comece pelo começo</h2>
+            </header>
+
+            <ul class="new-quizz-form">
+
+                <li class="new-quizz-input">
+                    <input class="new-quizz-input-field quizz-title" type="text" placeholder="Título do seu quizz">
+                </li>
+
+                <li class="new-quizz-input">
+                    <input class="new-quizz-input-field quizz-img" type="text" placeholder="URL da imagem do seu quizz">
+                </li>
+
+                <li class="new-quizz-input">
+                    <input class="new-quizz-input-field quizz-questions" type="text"
+                        placeholder="Quantidade de perguntas do quizz">
+                </li>
+
+                <li class="new-quizz-input">
+                    <input class="new-quizz-input-field quizz-level" type="text"
+                        placeholder="Quantidade de níveis do quizz">
+                </li>
+
+            </ul>
+
+            <button class="new-quizz-go-to-create-questions">Prosseguir para criar perguntas</button>
+        </article>
+    
+    `
+}
+
+//renderQuizzListPage()
