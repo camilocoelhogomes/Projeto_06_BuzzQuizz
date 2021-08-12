dda63ead8a6c8414d4eb28050462f253f950ff81
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-11 22:08:38 -0300

    page 3.2 mobile layout implemented and make the BuzzQuizz label position fixed

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,9 @@ function quizzListRenderAllQuizzes() {
 
 function quizzListSaveAllQuizzesAnswer(answer) {
     allQuizzes = answer.data;
-    quizzListRenderAllQuizzes();
+    if (document.querySelector('.style-page').href.includes('quizzList')) {
+        quizzListRenderAllQuizzes();
+    }
 }
 
 function quizzListGetAllQuizzes() {
@@ -73,7 +75,7 @@ function renderQuizzCreationPage() {
                 <h2 class="new-quizz-title">Comece pelo começo</h2>
             </header>
 
-            <ul class="new-quizz-form">
+            <ul class="new-quizz-form background-white">
 
                 <li class="new-quizz-input">
                     <input class="new-quizz-input-field quizz-title" type="text" placeholder="Título do seu quizz">
@@ -95,10 +97,98 @@ function renderQuizzCreationPage() {
 
             </ul>
 
-            <button class="new-quizz-go-to-create-questions">Prosseguir para criar perguntas</button>
+            <button onclick='renderQuizzCreationQuestionsPage()' class="new-quizz-go-to-create-questions">Prosseguir para criar perguntas</button>
         </article>
     
     `
 }
 
-//renderQuizzListPage()
+function quizzCreationQuestionToggleQuestion(element) {
+    olderQuestion = document.querySelector('.quis-creation-question-inputs.show');
+    olderQuestion.classList.toggle('show');
+    olderQuestion.parentNode.querySelector('.create-new-question-button.hide').classList.toggle('hide');
+    element.classList.toggle('hide');
+    element.parentNode.parentNode.querySelector('.quis-creation-question-inputs').classList.toggle('show');
+}
+
+function renderQuizzCreationQuestionFields() {
+    let questionsInputs = '';
+
+    for (i = 1; i <= 3; i++) {
+        questionsInputs += `
+            <li class='new-quizz-input'>
+
+                <header class="quizz-creation-question-header">
+                    <h3 class="quizz-creation-question-title">Pergunta ${i}</h3>
+            
+                    <button onclick="quizzCreationQuestionToggleQuestion(this)" class="create-new-question-button ${i === 1 ? 'hide' : ''}">
+                        <img class="create-new-question-img" src="./img/Vector.svg" alt="">
+                    </button>
+                    
+                </header>
+
+                <div class="quis-creation-question-inputs hide ${i === 1 ? 'show' : ''}">
+
+                    <div class="quizz-creation-question-pair-input">
+                        <input class="new-quizz-input-field quizz-question-text" type="text"
+                            placeholder="Texto da pergunta">
+                        <input class="new-quizz-input-field quizz-question-background" type="text"
+                            placeholder="Cor de fundo da pergunta">
+                    </div>
+
+                    <h3 class="quizz-creation-question-title">Resposta Correta</h3>
+
+                    <div class="quizz-creation-question-pair-input">
+                        <input class="new-quizz-input-field quizz-corret-awnser" type="text"
+                            placeholder="Resposta correta">
+                        <input class="new-quizz-input-field quizz-img" type="text" placeholder="URL da imagem">
+                    </div>
+
+                    <h3 class="quizz-creation-question-title">Respostas Incorretas</h3>
+
+                    <div class="quizz-creation-question-pair-input">
+                        <input class="new-quizz-input-field quizz-incorrect-awser" type="text"
+                            placeholder="Resposta incorreta 1">
+                        <input class="new-quizz-input-field quizz-img" type="text" placeholder="URL da imagem 1">
+                    </div>
+
+                    <div class="quizz-creation-question-pair-input">
+                        <input class="new-quizz-input-field quizz-incorrect-awser" type="text"
+                            placeholder="Resposta incorreta 2">
+                        <input class="new-quizz-input-field quizz-img" type="text" placeholder="URL da imagem 2">
+                    </div>
+
+                    <div class="quizz-creation-question-pair-input">
+                        <input class="new-quizz-input-field quizz-incorrect-awser" type="text"
+                            placeholder="Resposta incorreta 3">
+                        <input class="new-quizz-input-field quizz-img" type="text" placeholder="URL da imagem 3">
+                    </div>
+
+                </div>
+
+            </li>
+        `
+    }
+
+    document.querySelector(`.new-quizz-form`).innerHTML = questionsInputs;
+}
+
+function renderQuizzCreationQuestionsPage() {
+
+    document.querySelector('.style-page').href = './styles/quizzCreation.css'
+
+    document.querySelector('main').innerHTML = `
+        <header class="new-quizz-header">
+            <h2 class="new-quizz-title">Crie suas perguntas</h2>
+        </header>
+
+        <ul class="new-quizz-form">
+            
+        </ul>
+
+        <button class="new-quizz-go-to-create-questions">Prosseguir para criar Níveis</button>
+    `
+    renderQuizzCreationQuestionFields();
+}
+
+renderQuizzListPage();
